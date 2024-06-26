5d5b483c28362ec290028e56bde2f474bb72383d
Samriddhi Sinha <samriddhi@progno.io>
2024-06-27 02:48:47 +0530

    Added Faithfulness as a Metric

--- groqeval/metrics/context_relevance.py
+++ groqeval/metrics/context_relevance.py
@@ -22,20 +22,27 @@ class ContextRelevance(BaseMetric):
             f"Use the following JSON schema for your output: {json_representation}"
         )
 
-
     @property
     def relevance_prompt(self):
         return (
             f"Given the prompt: '{self.prompt}', evaluate the relevance of the following statements. "
-            "Score each coherent sentence on a scale from 1 to 10, where 1 means the sentence is completely irrelevant to the context and prompt, "
-            "and 10 means it is highly relevant. Ensure that the full range of scores is utilized, not just the two extremes, to prevent the scoring from being binary in nature. Include a rationale for each score to explain why the sentence received that rating. "            
+            "Score each coherent sentence on a scale from 1 to 10, where 1 means the sentence is completely irrelevant to the prompt, "
+            "and 10 means it is highly relevant. Ensure that the full range of scores is utilized, not just the two extremes, "
+            "to prevent the scoring from being binary in nature. Make sure that anything relevant to the prompt should score over 5." 
+            "Include a rationale for each score to explain why the sentence received that rating. "            
             f"Use the following JSON schema for your output: {json.dumps(ScoredContext.model_json_schema(), indent=2)}"
         )
+    
+    @property
+    def format_retrieved_context(self):
+        formatted_strings = "\n".join(f"- {s}" for s in self.context)
+        return f"The retrieved context includes the following items:\n{formatted_strings}"
+
 
     def context_decomposition(self):
         messages = [
             {"role": "system", "content": self.context_decomposition_prompt},
-            {"role": "user", "content": self.context}
+            {"role": "user", "content": self.format_retrieved_context}
         ]
         print(messages)
         response = self.groq_chat_completion(
--- groqeval/metrics/faithfulness.py
+++ groqeval/metrics/faithfulness.py
@@ -1,35 +1,43 @@
-# groqeval/metrics/answer_relevance.py
+# groqeval/metrics/faithfulness.py
 import json
 from groqeval.models.output import Output, ScoredOutput
 from groqeval.metrics.base_metric import BaseMetric
 
-class AnswerRelevance(BaseMetric):
-    def __init__(self, groq_client, output, prompt):
+class Faithfulness(BaseMetric):
+    def __init__(self, groq_client, context, output):
         super().__init__(groq_client)
+        self.context = context
         self.output = output
-        self.prompt = prompt
+
 
     @property
     def output_decomposition_prompt(self):
         json_representation = json.dumps(Output.model_json_schema(), indent=2)
         return (
             "Please process the following output from a language model and decompose it into individual phrases or chunks. "
-            "For each phrase or chunk, evaluate whether it can be considered a statement based on its form as a declarative construct "
-            "that communicates information, opinions, or beliefs. A phrase should be marked as a statement (true) if it forms a clear, standalone declaration. "
-            "Phrases that are overly vague, questions, or merely connective phrases without any declarative content should be marked as not statements (false). "
-            "Return the results in a JSON format. The JSON should have an array of objects, each representing a phrase with two properties: "
-            "a 'string' that contains the phrase text, and a 'flag' that is a boolean indicating whether the text is considered a statement (true) or not (false).\n"
+            "For each phrase or chunk, evaluate whether it can be considered a claim based on its form as a declarative construct "
+            "that communicates information, opinions, or beliefs. A phrase or chunk should be marked as a claim (true) if it forms a clear, standalone declaration, "
+            "conveying a specific assertion or point. Phrases or chunks that are overly vague, purely interrogative, or function as connective phrases without "
+            "substantial declarative content should be marked as not claims (false). "
+            "Return the results in a JSON format. The JSON should have an array of objects, each representing a phrase or chunk with two properties: "
+            "a 'string' that contains the text of the claim, and a 'flag' that is a boolean indicating whether the text is considered a claim (true) or not (false).\n"
             f"Use the following JSON schema for your output: {json_representation}"
         )
 
     @property
-    def relevance_prompt(self):
+    def format_retrieved_context(self):
+        formatted_strings = "\n".join(f"- {s}" for s in self.context)
+        return f"The retrieved context includes the following items:\n{formatted_strings}"
+    
+    @property
+    def faithfulness_prompt(self):
         return (
-            f"Given the prompt: '{self.prompt}', evaluate the relevance of the following statements. "
-            "Score each coherent statement on a scale from 1 to 10, where 1 means the statement is completely irrelevant to the prompt, "
-            "and 10 means it is highly relevant. Ensure that the full range of scores is utilized, not just the two extremes, "
-            "to prevent the scoring from being binary in nature. Make sure that anything relevant to the prompt should score over 5." 
-            "Include a rationale for each score to explain why the statement received that rating. "            
+            f"Given the context: '{self.format_retrieved_context}', evaluate the truthfulness of the following claims. "
+            "Score each claim on a scale from 1 to 10, where 1 means the claim is completely false or unsupported by the context, "
+            "and 10 means the claim is entirely true and supported by the context. Ensure that the full range of scores is utilized, not just the two extremes, "
+            "to prevent the scoring from being binary in nature. Make sure that any claim supported in the context should score over 5."
+            "Claims that are true but not supporrted by the context should score less than 5 but near to it"
+            "Include a rationale for each score to explain why the claim received that rating based on the facts presented in the context. "
             f"Use the following JSON schema for your output: {json.dumps(ScoredOutput.model_json_schema(), indent=2)}"
         )
 
@@ -47,14 +55,16 @@ class AnswerRelevance(BaseMetric):
         )
         return Output.model_validate_json(response.choices[0].message.content)
     
-    def score_relevance(self):
+
+    def score_faithfulness(self):
         decomposed_output = self.output_decomposition()
         # Filter out incoherent sentences
         coherent_sentences = [s for s in decomposed_output.sentences if s.flag]
         messages = [
-            {"role": "system", "content": self.relevance_prompt},
+            {"role": "system", "content": self.faithfulness_prompt},
             {"role": "user", "content": json.dumps({"sentences": [s.string for s in coherent_sentences]}, indent=2)}
         ]
+        print(messages)
         response = self.groq_chat_completion(
             messages=messages,
             model="llama3-70b-8192",
@@ -64,7 +74,7 @@ class AnswerRelevance(BaseMetric):
         return ScoredOutput.model_validate_json(response.choices[0].message.content)
     
     def score(self):
-        scored_output = self.score_relevance()
+        scored_output = self.score_faithfulness()
         if scored_output.scores:
             average_score = sum([output.score for output in scored_output.scores]) / len(scored_output.scores)
             return {
